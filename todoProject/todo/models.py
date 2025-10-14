fe98cd8f05ee6d07907c0fc69e3e89906fa468b6
denis-spe <denisbrian07@gmail.com>
2025-10-14 22:49:24 +0200

    Add Activities model and initial migration for the todo app

--- todoProject/todo/models.py
+++ todoProject/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Activities(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=260)
+    published_time = models.DateTimeField()
+
+    def __str__(self):
+        return "%s" % self.title
