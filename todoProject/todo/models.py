cdef354aa83e9846c3e2b3cbc55b3e4334b56ee7
denis-spe <denisbrian07@gmail.com>
2025-10-28 20:02:53 +0200

    Refactor models and tests: add ActivatyChoices, enhance Activaties model, and implement tests for validation

--- todoProject/todo/models.py
+++ todoProject/todo/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
+class ActivatyChoices(models.TextChoices):
+    TASK = "TK", "Task"
+    HABIT = "HT", "Habit"
 
 # Create your models here.
-class Activities(models.Model):
+class Activaties(models.Model):
     title = models.CharField(max_length=150)
-    description = models.CharField(max_length=260)
-    published_time = models.DateTimeField()
+    description = models.CharField(max_length=260, default="")
+    is_activate_done = models.BooleanField(default=False)
+    activate_type = models.CharField(max_length=6, choices=ActivatyChoices)
+    published_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return "%s" % self.title
--- todoProject/todo/tests.py
+++ todoProject/todo/tests.py
@@ -1,3 +1,36 @@
+# Love the LORD your GOD with all mind and with all your soul and with 
+# all your might. And Love your neighbor as your self.
 from django.test import TestCase
+from .models import Activaties
+from django.utils import timezone
+from datetime import datetime
+from django.core.exceptions import ValidationError
+
+def activaties(
+        title: str = "Coding",
+        activate_type: str = "TK",
+        description: str = "Writing code for programming",
+        is_activate_done: bool = False,
+        published_time: datetime = timezone.now()
+):
+    act = Activaties(
+        title = title,
+        activate_type = activate_type,
+        description = description,
+        is_activate_done = is_activate_done,
+        published_time = published_time
+
+    )
+    act.clean_fields()
+    act.save()
 
 # Create your tests here.
+class TestActivaties(TestCase):
+    def test_if_activate_is_not_empty(self):
+        activaties()
+        act_count = Activaties.objects.count()
+        self.assertGreater(act_count, 0)
+
+    def test_if_activate_throw_error_on_wrong_activate_type(self):
+        with self.assertRaises(ValidationError):
+            activaties(activate_type="act")
